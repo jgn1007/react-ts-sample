b123911ba57b7fe7bd9563bee8b041f525d38e4e
kin <kokunan.kin@ari-jp.com>
2021-09-13 13:29:15 +0900

    arrange comment

--- src/sagas/root_saga.ts
+++ src/sagas/root_saga.ts
@@ -1,9 +1,4 @@
-/**
- * *************************************************
- * sagasのまとめ
- *
- * *************************************************
- */
+
 import { takeLatest, takeEvery, all, fork } from 'redux-saga/effects'
 import { addTodo } from '../actions/todo_action';
 import todoSaga from './todo_saga';
